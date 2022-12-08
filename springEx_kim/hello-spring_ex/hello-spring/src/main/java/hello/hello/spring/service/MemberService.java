0730c540a49ccb48e9866614ad1413544377c7c0
suji5262 <suji5262@gmail.com>
2022-12-08 19:37:23 +0900

    22.12.08

---
+++ springEx_kim/hello-spring_ex/hello-spring/src/main/java/hello/hello/spring/service/MemberService.java
@@ -0,0 +1,46 @@
+package hello.hello.spring.service;
+
+import hello.hello.spring.domain.Member;
+import hello.hello.spring.repository.MemberRepository;
+import hello.hello.spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+
+        /*
+        회원가입
+        * */
+//        Optional<Member> result = memberRepository.findByName(member.getName()); // Optional 를 바로 반환하는게 좋지 않음
+
+        // 같은 이름이 있는 중복 회원 x
+        validateDuplicateMember(member); // 중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    /*
+    * 전체회원조회
+    * */
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}

517a688a0a8aea72a2aaf937ebee99d857298f67
suji5262 <suji5262@gmail.com>
2023-01-20 22:05:50 +0900

    feat: 23.01.20 공부파일 upload

--- spring_workbook/CH4/springex/src/main/java/org/zerock/springex/mapper/TodoMapper.java
+++ spring_workbook/CH4/springex/src/main/java/org/zerock/springex/mapper/TodoMapper.java
@@ -1,6 +1,7 @@
 package org.zerock.springex.mapper;
 
 import org.zerock.springex.domain.TodoVO;
+import org.zerock.springex.dto.PageRequestDTO;
 
 import java.util.List;
 
@@ -17,4 +18,10 @@ public interface TodoMapper {
     void delete(Long tno); // 삭제 기능
 
     void update(TodoVO todoVO); // 수정 기능
+
+    List<TodoVO> selectList(PageRequestDTO pageRequestDTO); // PageRequestDTO 를 파라미터로 처리
+
+    int getCount(PageRequestDTO pageRequestDTO);
+    // 화면 페이지 번호들을 구성하기 위해 -> 전체 데이터의 수를 알아야됨
+    // 나중에 검색을 대비해 PageRequestDTO를 파라미터로 받도록 처리
 }

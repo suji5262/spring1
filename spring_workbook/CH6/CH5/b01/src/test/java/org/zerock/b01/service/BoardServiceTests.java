e0498886971de776f22130dc27fdeebce2b1e39c
suji5262 <suji5262@gmail.com>
2023-01-29 23:53:28 +0900

    feat: 23.01.29 공부파일 upload

---
+++ spring_workbook/CH6/CH5/b01/src/test/java/org/zerock/b01/service/BoardServiceTests.java
@@ -0,0 +1,64 @@
+package org.zerock.b01.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.b01.dto.BoardDTO;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
+
+@SpringBootTest
+@Log4j2
+
+public class BoardServiceTests {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Test
+    public void testRegister() {
+
+        log.info(boardService.getClass().getName());
+
+        BoardDTO boardDTO = BoardDTO.builder()
+                .title("Sample title.......")
+                .content("Sample Content....")
+                .writer("user00")
+                .build();
+
+        Long bno = boardService.register(boardDTO);
+
+        log.info("bno : " + bno);
+    }
+
+    @Test
+    public void testModify() {
+
+        // 변경에 필요한 데이터만
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(1000L)
+                .title("Updated... 1000")
+                .content("Updated content... 1000")
+                .build();
+
+        boardService.modify(boardDTO);
+    }
+
+    @Test
+    public void testList() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .type("twc")
+                .keyword("1")
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+        log.info(responseDTO);
+    }
+
+}
+

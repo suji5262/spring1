b2d2c2e1aa0b56a53e3c62297b1650282a43fc59
suji5262 <suji5262@gmail.com>
2023-02-22 01:42:52 +0900

    test: MemoryMemberRepositoryTest 파일 수정

--- springEx_kim/hello1/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ springEx_kim/hello1/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,16 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-import org.assertj.core.api.Assertions;
-
-//import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemoryMemberRepositoryTest {
 
@@ -68,11 +64,4 @@ public class MemoryMemberRepositoryTest {
 
         assertThat(result.size()).isEqualTo(2);
     }
-
-
-
-
-
-
-
 }

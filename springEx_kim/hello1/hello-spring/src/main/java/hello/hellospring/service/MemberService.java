bef3f8f48f97d5c1060e36422b5a647732afacc6
suji5262 <suji5262@gmail.com>
2023-02-25 00:01:30 +0900

    Docs: MemberService 파일수정 - @Transactional 어노테이션 추가 - MemberService() 수정

--- springEx_kim/hello1/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ springEx_kim/hello1/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,15 +2,18 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 //@Service
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
-//    @Autowired
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     } // 외부에서 넣어주도록

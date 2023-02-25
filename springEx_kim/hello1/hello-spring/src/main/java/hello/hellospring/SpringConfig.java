48b52e6815325d0e3b0aa970ae83c84b1b3d7212
suji5262 <suji5262@gmail.com>
2023-02-26 00:44:41 +0900

    Comment: 시간 측정 로직 주석처리

--- springEx_kim/hello1/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ springEx_kim/hello1/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,32 +1,25 @@
 package hello.hellospring;
-
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
 
-    private DataSource dataSource;
-
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
-    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 }

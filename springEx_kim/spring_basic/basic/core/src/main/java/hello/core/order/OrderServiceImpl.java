14b99a86aa6cf8c1f373a53e32b3dceda53c8439
suji5262 <suji5262@gmail.com>
2023-03-07 21:24:04 +0900

    Docs: MemoryMemberRepository file 수정 - @Component 어노테이션 추가 - 공부한 부분 주석처리

--- springEx_kim/spring_basic/basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ springEx_kim/spring_basic/basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,26 +1,90 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
-    // 회원 찾기
     private final DiscountPolicy discountPolicy;
+
+//    private final MemberRepository memberRepository;
+//    // 회원 찾기
+//    private final DiscountPolicy discountPolicy;
+
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
 
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    // 할인 정책(고정)
 
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
+
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy
+//            discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    } // 일반 메서드 주입
+
+
+//    @Autowired
+//    public OrderServiceImpl(
+//            MemberRepository memberRepository,
+//            DiscountPolicy discountPolicy) { // 생성자 주입
+////        System.out.println("memberRepository = " + memberRepository);
+////        System.out.println("discountPolicy = " + discountPolicy);
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+//    @Autowired
+//    public OrderServiceImpl(
+//            MemberRepository memberRepository,
+//            @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+////        System.out.println("memberRepository = " + memberRepository);
+////        System.out.println("discountPolicy = " + discountPolicy);
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    } // @Qualifier
+
+
+    @Autowired
     public OrderServiceImpl(
             MemberRepository memberRepository,
-            DiscountPolicy discountPolicy) { // 생성자 주입
+            @MainDiscountPolicy DiscountPolicy discountPolicy) { // 생성자 주입
+//        System.out.println("memberRepository = " + memberRepository);
+//        System.out.println("discountPolicy = " + discountPolicy);
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    } // 어노테이션 직접 만들기
+
+
+
+//    @Autowired(required = false) // 필수값이 있어도 되고 없어도 된다
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        System.out.println("memberRepository = " + memberRepository);
+//        this.memberRepository = memberRepository;
+//    }
+
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+//    // 수정자 주입
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -31,4 +95,8 @@ public class OrderServiceImpl implements OrderService {
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
